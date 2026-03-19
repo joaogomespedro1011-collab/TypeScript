c1c486af17a1ed1d4bd72fc5df583c0b22362751
João Pedro <joaogomespedro1011@gmail.com>
2026-03-19 11:25:11 -0300

    ConfiguraçãoDaAPI

---
+++ book-app/src/services/api.ts
@@ -0,0 +1,33 @@
+const BASE_URL = " ";
+
+export const getBooks = async () => {
+  const res = await fetch(BASE_URL);
+  return res.json();
+};
+
+export const addBook = async (book: any) => {
+  const res = await fetch(BASE_URL, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(book)
+  });
+  return res.json();
+};
+
+export const deleteBook = async(id : string ) => {
+    await fetch(`${BASE_URL}/${id}` , {
+        method : "DELETE"
+    });
+};
+
+export const updateBook = async ( id: string, book: any) => {
+    await fetch(`${BASE_URL}/${id}` , {
+        method : "PUT",
+        headers: {
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(book)
+    });
+};
